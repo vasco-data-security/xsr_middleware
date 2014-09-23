e2a05c672ff1af500656f9425b8c5a3020f02b71
ddf+kde <dimitri.defrenne@vasco.com>
2014-09-23 16:55:20 +0200

    Remove debugging info

--- lib/xsr_middleware.rb
+++ lib/xsr_middleware.rb
@@ -10,30 +10,22 @@ class XsrMiddleware
     request = Rack::Request.new(env)
 
     if request.cookies['_dpplus_xsr_id']
-      Rails.logger.debug "\n=====================\nhas cookie! mmm cookies\n====================="
       XsrMiddleware::RequestContext.set_tracking_id(request.cookies['_dpplus_xsr_id'], hashed: true)
     elsif request.env['X-TrackingId']
-      Rails.logger.debug "\n=====================\nhas X-TrackingId\n====================="
       XsrMiddleware::RequestContext.set_tracking_id(request.env['X-TrackingId'], hashed: true)
     else
-      Rails.logger.debug "\n=====================\nno has cookie or header!\n====================="
-      Rails.logger.debug "\n=====================\nsession options: #{request.session_options.inspect}\n====================="
       XsrMiddleware::RequestContext.set_tracking_id(request.session_options[:id], hashed: false)
     end
 
     XsrMiddleware::RequestContext.request_id = XsrMiddleware::RequestContext.generate_token
     XsrMiddleware::RequestContext.set_default_operator if Module.constants.include? :MdpBackoffice
 
-    Rails.logger.info("Request #{request.request_method.to_s.upcase} #{request.path} from #{request.ip}")
+    # Rails.logger.info("Request #{request.request_method.to_s.upcase} #{request.path} from #{request.ip}")
 
     status, headers, body = @app.call(env)
 
-    Rails.logger.debug "\n=====================\nXSR tracking_id: #{XsrMiddleware::RequestContext.tracking_id}\n====================="
-
     headers['X-RequestId']  = XsrMiddleware::RequestContext.request_id
     headers['X-TrackingId'] = XsrMiddleware::RequestContext.tracking_id
-    Rails.logger.debug "\n=====================\nheader: #{headers['X-RequestId']}\n====================="
-    Rails.logger.debug "\n=====================\nheader: #{headers['X-TrackingId']}\n====================="
 
     if XsrMiddleware::RequestContext.tracking_id
       Rack::Utils.set_cookie_header!(headers, "_dpplus_xsr_id", { value: XsrMiddleware::RequestContext.tracking_id,
@@ -42,7 +34,7 @@ class XsrMiddleware
       Rack::Utils.delete_cookie_header!(headers, "_dpplus_xsr_id", { path: '/' })
     end
 
-    Rails.logger.info("Response #{status} #{headers['Content-Type']} #{headers['Content-Length']}")
+    # Rails.logger.info("Response #{status} #{headers['Content-Type']} #{headers['Content-Length']}")
 
     XsrMiddleware::RequestContext.reset
 
