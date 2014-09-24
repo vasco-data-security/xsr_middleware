5070ba70e77d1eb752a15995027f9b7c432e6f70
ddf+kde <dimitri.defrenne@vasco.com>
2014-09-24 10:04:18 +0200

    Specs for RequestContext

---
+++ spec/lib/xsr_middleware/request_context_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+describe XsrMiddleware::RequestContext do
+
+  subject { XsrMiddleware::RequestContext }
+
+  describe "#create_tracking_id" do
+    it "adds a new tracking id to the Thread" do
+      subject.create_tracking_id
+      expect(Thread.current['ctx_tracking_id']).to match /[a-z0-9]{32}/
+    end
+  end
+
+  describe "#set_tracking_id" do
+    it "adds the tracking id to the Thread" do
+      subject.set_tracking_id('123456')
+      expect(Thread.current['ctx_tracking_id']).to eql '123456'
+    end
+  end
+
+  describe "#reset_tracking_id!" do
+    it "removes the tracking id from the Thread" do
+      Thread.current['ctx_tracking_id'] = '1234567890'
+      subject.reset_tracking_id!
+      expect(Thread.current['ctx_tracking_id']).to be_nil
+    end
+  end
+
+  describe "#tracking_id" do
+    it "returns the tracking id which is stored in the Thread" do
+      Thread.current['ctx_tracking_id'] = '1234567890'
+      expect(subject.tracking_id).to eql "1234567890"
+    end
+  end
+end
