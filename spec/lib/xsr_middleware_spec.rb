1a74c454999ed7d38f3d7dfccd61531593491927
ddf+kde <dimitri.defrenne@vasco.com>
2014-09-24 15:13:25 +0200

    Get rid of a duplicate X-RequestId header...
    Rails also sets X-Request-Id when not yet defined.
    Also brought X-Tracking-Id in line with this.

--- spec/lib/xsr_middleware_spec.rb
+++ spec/lib/xsr_middleware_spec.rb
@@ -9,33 +9,33 @@ describe XsrMiddleware do
 
   subject { XsrMiddleware.new(app) }
 
-  context "when there is no X-TrackingId-header" do
+  context "when there is no X-Tracking-Id-header" do
     it "uses the session_id to generate a tracking_id" do
       response = subject.call(env)
 
-      expect(response[1]["X-TrackingId"]).to eql(Digest::MD5.new.hexdigest('12345'))
+      expect(response[1]["X-Tracking-Id"]).to eql(Digest::MD5.new.hexdigest('12345'))
     end
 
-    it "sets the X-TrackingId-header" do
+    it "sets the X-Tracking-Id-header" do
       response = subject.call(env)
 
-      expect(response[1]["X-TrackingId"]).to match /[a-z0-9]{32}/
+      expect(response[1]["X-Tracking-Id"]).to match /[a-z0-9]{32}/
     end
 
     context "when there is no session_id" do
       it "uses an encoded empty string" do
         response = subject.call({})
 
-        expect(response[1]["X-TrackingId"]).to eql eql(Digest::MD5.new.hexdigest(''))
+        expect(response[1]["X-Tracking-Id"]).to eql(Digest::MD5.new.hexdigest(''))
       end
     end
   end
 
-  context "when X-TrackingId-header is present" do
+  context "when X-Tracking-Id-header is present" do
     it "doesn't change its value" do
-      response = subject.call(env.merge({ 'X-TrackingId' => '57a85d228a17a995a73ab4149c120ef0' }))
+      response = subject.call(env.merge({ 'X-Tracking-Id' => '57a85d228a17a995a73ab4149c120ef0' }))
 
-      expect(response[1]["X-TrackingId"]).to eql('57a85d228a17a995a73ab4149c120ef0')
+      expect(response[1]["X-Tracking-Id"]).to eql('57a85d228a17a995a73ab4149c120ef0')
     end
   end
 end
