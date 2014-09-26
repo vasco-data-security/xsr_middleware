42c724984de939c7cd06de270c0a7dc28776df00
ddf+kde <dimitri.defrenne@vasco.com>
2014-09-26 11:02:21 +0200

    Follow naming conventions for header fields..
    We do this because Rack modifies the header internally.
    Don't be confused, Rack does some weird and annoying magic...
    
    Naming header fields:
    
    "Meta-variables with names beginning with "HTTP_" contain values read
    from the client request header fields, if the protocol used is HTTP.
    The HTTP header field name is converted to upper case, has all
    occurrences of "-" replaced with "_" and has "HTTP_" prepended
    to give the meta-variable name."
    
    Source: https://tools.ietf.org/html/rfc3875#section-4.1.18

--- lib/xsr_middleware.rb
+++ lib/xsr_middleware.rb
@@ -3,9 +3,12 @@ require 'digest'
 
 ##
 # This middleware helps with tracking users accross multiple requests and services (anonymously):
-#   - by setting a request id to follow a request to multiple services (X-MDP-Request-Id)
-#   - by setting a tracking id to follow a user through multiple requests (X-Tracking-Id)
+#   - by setting a request id to follow a request to multiple services (X-Mdp-Request-Id)
+#   - by setting a tracking id to follow a user through multiple requests (X-Mdp-Tracking-Id)
 #
+# Be aware that whenever you perform an actual call to a web server,
+# Rack automatically prepends "HTTP_" to the header's key, replaces dashes by
+# underscores and capitalizes word segments.
 
 class XsrMiddleware
 
@@ -16,15 +19,15 @@ class XsrMiddleware
   def call(env)
     request = Rack::Request.new(env)
 
-    if request.env['X-Tracking-Id']
-      XsrMiddleware::RequestContext.set_tracking_id(request.env['X-Tracking-Id'])
+    if request.env['HTTP_X_MDP_TRACKING_ID']
+      XsrMiddleware::RequestContext.set_tracking_id(request.env['HTTP_X_MDP_TRACKING_ID'])
     else
       session_id = request.session_options[:id] || ''
       XsrMiddleware::RequestContext.set_tracking_id(encode_string(session_id))
     end
 
-    if request.env['X-MDP-Request-Id']
-      XsrMiddleware::RequestContext.set_mdp_request_id(request.env['X-MDP-Request-Id'])
+    if request.env['HTTP_X_MDP_REQUEST_ID']
+      XsrMiddleware::RequestContext.set_mdp_request_id(request.env['HTTP_X_MDP_REQUEST_ID'])
     else
       XsrMiddleware::RequestContext.set_mdp_request_id(encode_string("#{$$}#{request.path}#{Time.now.to_s}"))
     end
@@ -33,8 +36,10 @@ class XsrMiddleware
 
     status, headers, body = @app.call(env)
 
-    headers['X-MDP-Request-Id'] = XsrMiddleware::RequestContext.mdp_request_id
-    headers['X-Tracking-Id'] = XsrMiddleware::RequestContext.tracking_id
+    # See the description at the top of this file.
+    # Rack does some annoying magic!
+    headers['X-Mdp-Request-Id'] = XsrMiddleware::RequestContext.mdp_request_id
+    headers['X-Mdp-Tracking-Id'] = XsrMiddleware::RequestContext.tracking_id
 
     XsrMiddleware::RequestContext.reset
 
--- spec/lib/xsr_middleware_spec.rb
+++ spec/lib/xsr_middleware_spec.rb
@@ -9,49 +9,49 @@ describe XsrMiddleware do
 
   subject { XsrMiddleware.new(app) }
 
-  context "when there is no X-Tracking-Id-header" do
+  context "when there is no X-Mdp-Tracking-Id-header" do
     it "uses the session_id to generate a tracking_id" do
       response = subject.call(env)
 
-      expect(response[1]["X-Tracking-Id"]).to eql(Digest::MD5.new.hexdigest('12345'))
+      expect(response[1]["X-Mdp-Tracking-Id"]).to eql(Digest::MD5.new.hexdigest('12345'))
     end
 
-    it "sets the X-Tracking-Id-header" do
+    it "sets the X-Mdp-Tracking-Id-header" do
       response = subject.call(env)
 
-      expect(response[1]["X-Tracking-Id"]).to match /[a-z0-9]{32}/
+      expect(response[1]["X-Mdp-Tracking-Id"]).to match /[a-z0-9]{32}/
     end
 
     context "when there is no session_id" do
       it "uses an encoded empty string" do
         response = subject.call({})
 
-        expect(response[1]["X-Tracking-Id"]).to eql(Digest::MD5.new.hexdigest(''))
+        expect(response[1]["X-Mdp-Tracking-Id"]).to eql(Digest::MD5.new.hexdigest(''))
       end
     end
   end
 
-  context "when X-Tracking-Id-header is present" do
+  context "when X-Mdp-Tracking-Id-header is present" do
     it "doesn't change its value" do
-      response = subject.call(env.merge({ 'X-Tracking-Id' => '57a85d228a17a995a73ab4149c120ef0' }))
+      response = subject.call(env.merge({ 'HTTP_X_MDP_TRACKING_ID' => '57a85d228a17a995a73ab4149c120ef0' }))
 
-      expect(response[1]["X-Tracking-Id"]).to eql('57a85d228a17a995a73ab4149c120ef0')
+      expect(response[1]["X-Mdp-Tracking-Id"]).to eql('57a85d228a17a995a73ab4149c120ef0')
     end
   end
 
-  context "when there is no X-MDP-Request-Id-header" do
+  context "when there is no X-Mdp-Request-Id-header" do
     it "sets the header" do
       response = subject.call({})
 
-      expect(response[1]["X-MDP-Request-Id"]).to match /[a-z0-9]{32}/
+      expect(response[1]["X-Mdp-Request-Id"]).to match /[a-z0-9]{32}/
     end
   end
 
-  context "when there is a X-MDP-Request-Id-header" do
+  context "when there is a X-Mdp-Request-Id-header" do
     it "reuses the header's value" do
-      response = subject.call(env.merge({ 'X-MDP-Request-Id' => '800eaa6f53e678602fb0266a1b037179' }))
+      response = subject.call(env.merge({ 'HTTP_X_MDP_REQUEST_ID' => '800eaa6f53e678602fb0266a1b037179' }))
 
-      expect(response[1]["X-MDP-Request-Id"]).to eql('800eaa6f53e678602fb0266a1b037179')
+      expect(response[1]["X-Mdp-Request-Id"]).to eql('800eaa6f53e678602fb0266a1b037179')
     end
   end
 end
